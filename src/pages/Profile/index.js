f802637126fafff81ff081a719d75e9476692c84
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-10 22:08:22 -0300

    avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,49 +1,43 @@
 import React from 'react';
-
-import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
-
-import AvatarInput from './AvatarInput/index';
-
-import { signOut } from '~/store/modules/auth/actions';
-import { updateProfileRequest } from '~/store/modules/user/actions';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container } from './styles';
+import AvatarInput from './AvatarInput';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
   const dispatch = useDispatch();
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
   }
-  function handleSignOut() {
-    dispatch(signOut());
-  }
-
-  const profile = useSelector(state => state.user.profile);
 
   return (
     <Container>
-      <Form onSubmit={handleSubmit} initialData={profile}>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <AvatarInput name="avatar_id" />
-        <Input name={'name'} placeholder="Seu nome completo" />
-        <Input name="email" placeholder="Seu endereço de email" />
+
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço completo" />
         <hr />
         <Input
-          name="oldPassword"
           type="password"
+          name="oldPassword"
           placeholder="Sua senha atual"
         />
-        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input type="password" name="password" placeholder="Nova senha" />
         <Input
-          name="confirmPassword"
           type="password"
+          name="confirmPassword"
           placeholder="Confirmação de senha"
         />
-        <button type="submit">Atualizar</button>
+
+        <button type="submit">Atualizar perfil</button>
       </Form>
-      <button onClick={handleSignOut} type="button">
-        Sair
-      </button>
+
+      <button type="submit">Sair do Gobarber</button>
     </Container>
   );
 }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,29 +1,29 @@
-import { all, call, put, takeLatest } from "redux-saga/effects";
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
 
-import api from "../../../services/api";
+import api from '../../../services/api';
 
-import { updateProfileSuccess, updateProfileFailure } from "./actions";
+import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { email, name, ...rest } = payload.data;
+    const { email, name, avatar_id, ...rest } = payload.data;
 
     const profile = Object.assign(
-      { email, name },
+      { email, name, avatar_id },
       rest.oldPassword ? rest : {}
     );
 
-    const response = yield call(api.put, "users", profile);
+    const response = yield call(api.put, 'users', profile);
 
-    toast.success("Atualizado com sucesso");
+    toast.success('Atualizado com sucesso');
 
     yield put(updateProfileSuccess(response.data));
   } catch (err) {
-    toast.error("Revise seus dados");
+    toast.error('Revise seus dados');
     yield put(updateProfileFailure());
   }
 }
 
-export default all([takeLatest("@user/UPDATE_REQUEST", updateProfile)]);
+export default all([takeLatest('@user/UPDATE_REQUEST', updateProfile)]);
