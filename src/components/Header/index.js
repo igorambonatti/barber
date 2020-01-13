7a4ea3c962a1ac900e3bb71e157b2d9f2e25ca0c
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-13 20:52:27 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,10 @@ import logo from '~/assets/logo-icon.svg';
 
 import { Container, Content, Profile } from './styles';
 
+import { useSelector } from 'react-redux';
+
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -19,11 +22,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Igor Ambonatti</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Igor Ambonatti"
             />
           </Profile>
